415f9585e0162af05c9dd435d82e1e771e9247a0
ellen <ellenchi703@gmail.com>
2026-01-12 01:08:55 -0500

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'script',
+            includeAssets: ['logo.png'],
+            manifest: {
+                name: 'GL Design+Build',
+                short_name: 'GL Design',
+                description: 'Crafting Excellence in Design and Build',
+                theme_color: '#000000',
+                background_color: '#000000',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'logo.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'logo.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
 })
